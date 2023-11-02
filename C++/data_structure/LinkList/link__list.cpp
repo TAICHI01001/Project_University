f515d398d042080039f3f61aee330257e233ca11
TaiChi <anothai.0978452316@gmail.com>
2023-11-02 21:48:25 +0700

    first commit create node Link_list

---
+++ C++/data_structure/LinkList/link__list.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    int data;
+    Node *next;
+
+    Node(int value):data(value),next(NULL){}
+};
+
+int main(){
+    cout<<"----------------------------------------- \n";
+    cout<<"We will learn the data structure Lik_List \n";
+    cout<<"----------------------------------------- \n";
+}
